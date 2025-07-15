ab06e76853d187fb482e830669a324e7a4139e68
Hemanth Gokavarapu <5942165+hemanthgk10@users.noreply.github.com>
2025-07-15 19:57:29 +0530

    Update nat-server.tf

--- cloud-config/nat-server.tf
+++ cloud-config/nat-server.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "nat" {
   key_name = "${aws_key_pair.deployer.key_name}"
   source_dest_check = false
   tags = {
-    Name = "nat server"
+    Name = "nat server2"
   }
   connection {
     user = "ubuntu"
